7886e439058d67da95da92da71424b316161955f
ssb-ae-git <arerasani@gmail.com>
2026-03-23 10:37:19 -0400

    updated application java file again 9

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -1,41 +1,11 @@
 package com.demo;
 
-import com.sun.net.httpserver.HttpExchange;
-import com.sun.net.httpserver.HttpHandler;
-import com.sun.net.httpserver.HttpServer;
-
-import java.io.OutputStream;
-import java.net.InetSocketAddress;
-
 public class HelloWorld {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-        HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", 8000), 0);
+        System.out.println("Hello from Jenkins CI Pipeline!");
 
-        server.createContext("/", new HttpHandler() {
-
-            public void handle(HttpExchange exchange) {
-
-                try {
-
-                    String response = "Hello from Jenkins CI/CD Pipeline!";
-
-                    exchange.sendResponseHeaders(200, response.length());
-
-                    OutputStream os = exchange.getResponseBody();
-                    os.write(response.getBytes());
-                    os.close();
-
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
-        server.setExecutor(null);
-        server.start();
-
-        System.out.println("Server started on port 8000");
     }
+
 }
