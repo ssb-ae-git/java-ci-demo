2d42f8524f67476167f7ae1fd733468e4fefbe32
ssb-ae-git <arerasani@gmail.com>
2026-03-22 22:27:37 -0400

    updated application java file again 6

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -5,7 +5,6 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 import java.io.OutputStream;
-import java.net.InetAddress;
 import java.net.InetSocketAddress;
 import java.time.LocalDateTime;
 
@@ -13,10 +12,10 @@ public class HelloWorld {
 
     public static void main(String[] args) throws Exception {
 
-        String version = "Version 3 - CI/CD Live Demo";
+        String version = "Version 2 - Deployed via Jenkins CI/CD";
         String deployTime = LocalDateTime.now().toString();
-        String hostname = InetAddress.getLocalHost().getHostName();
 
+        // Bind to all interfaces so it is accessible externally
         HttpServer server = HttpServer.create(new InetSocketAddress("0.0.0.0", 8000), 0);
 
         server.createContext("/", new HttpHandler() {
@@ -29,15 +28,9 @@ public class HelloWorld {
                     String userAgent = exchange.getRequestHeaders().getFirst("User-Agent");
 
                     String response =
-                            "===== Jenkins CI/CD Demo Application =====\n\n" +
-                            "Hello from Jenkins Pipeline!\n\n" +
-                            "Application Version    : " + version + "\n" +
-                            "Deployment Time        : " + deployTime + "\n" +
-                            "Server Hostname        : " + hostname + "\n\n" +
-                            "Request Details\n" +
-                            "--------------\n" +
-                            "Requested URL          : " + requestURI + "\n" +
-                            "User Agent             : " + userAgent + "\n";
+                            "Hello from Jenkins CI/CD Pipeline\n" +
+                            version + "\n" +
+                            "Deployment Time        : " + deployTime + "\n";
 
                     exchange.sendResponseHeaders(200, response.length());
 
@@ -57,6 +50,5 @@ public class HelloWorld {
         System.out.println("Server started on port 8000");
         System.out.println("Application Version: " + version);
         System.out.println("Deployment Time: " + deployTime);
-        System.out.println("Hostname: " + hostname);
     }
 }
