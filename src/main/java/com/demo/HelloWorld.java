0c6fe57707940aaf555d755b78b4b9610c4a3613
ssb-ae-git <arerasani@gmail.com>
2026-04-13 16:44:52 -0400

    Modified the applocation code - 2

--- src/main/java/com/demo/HelloWorld.java
+++ src/main/java/com/demo/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello from Jenkins CI/CD Pipeline-1!");
+        System.out.println("Hello from Jenkins CI/CD Pipeline!");
 
     }
 
